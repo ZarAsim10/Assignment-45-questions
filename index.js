76d1292fb6e754997ec6da3f879fcf495f024485
ZarAsim10 <zara.salah10@gmail.com>
2024-02-27 10:42:16 +0500

    first assignment

---
+++ index.js
@@ -0,0 +1,4 @@
+var massage = "Zara!";
+console.log(massage);
+massage = "123";
+console.log(massage);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let massage: string = "Zara!";
+console.log(massage);
+massage = "123";
+console.log(massage);
